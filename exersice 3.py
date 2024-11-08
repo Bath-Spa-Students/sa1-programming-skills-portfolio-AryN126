af933010edb4d59a13e888a124ac2e36bfa33f0a
AryN126 <aryanbsu132@gmail.com>
2024-11-08 15:40:47 +0400

    Create exersice 3.py
    Assessment 1 - Programming-Skills-Portfolio

---
+++ exersice 3.py
@@ -0,0 +1,12 @@
+Data ={"name": "Aryan","age": 18,"hometown": "Dubai"}
+print(Data["name"])
+print(Data["age"])
+print(Data["hometown"])
+#advanced requirement
+name=input("please enter your name:")
+age=input("please enter your age:")
+hometown=input("please enter your hometown:")
+Data ={"name": name,"age": age,"hometown":hometown}
+print(Data["name"])
+print(Data["age"])
+print(Data["hometown"])
